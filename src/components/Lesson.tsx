236211fa2a5794ebd50a99fda669c8b4eb8a5931
Pedro <pedro-gomes17@hotmail.com>
2022-06-21 21:31:31 -0300

    feat: create new Lesson component

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,52 @@
+import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const isLessonAvailable = isPast(availableAt);
+  const availableDateFormatted = format(
+    availableAt,
+    "EEEE' • 'd' de 'MMMM ' • 'k'h'mm",
+    { locale: ptBR }
+  );
+
+  const classFormat = {
+    live: "AO VIVO",
+    class: "AULA PRÁTICA",
+  };
+
+  return (
+    <a href="#">
+      <span className="text-gray-300">{availableDateFormatted}</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
+          <span className="text-xs rounded py-[2px] px-2 text-white border border-green-300 font-bold">
+            {classFormat[type]}
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">{title}</strong>
+      </div>
+    </a>
+  );
+}
