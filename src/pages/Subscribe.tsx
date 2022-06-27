7cd931701f5b947e7eeda082b9b259b885305d38
Pedro <pedro-gomes17@hotmail.com>
2022-06-26 22:27:45 -0300

    improvement: added create subcribe function with mutation

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,38 @@
+import { gql, useMutation } from "@apollo/client";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
+
 export function Subscribe() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [createSubscriber, { loading }] = useMutation(
+    CREATE_SUBSCRIBER_MUTATION
+  );
+
+  async function handleCreateSubscribe(event: FormEvent) {
+    event.preventDefault();
+
+    await createSubscriber({
+      variables: {
+        name,
+        email,
+      },
+    });
+
+    navigate("/event");
+  }
+
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -24,21 +56,27 @@ export function Subscribe() {
             Inscreva-se gratuitamente
           </strong>
 
-          <form action="" className="flex flex-col gap-2 w-full">
+          <form
+            onSubmit={handleCreateSubscribe}
+            className="flex flex-col gap-2 w-full"
+          >
             <input
               type="text"
               placeholder="Seu nome completo"
               className="bg-gray-900 roundex px-5 h-14"
+              onChange={(event) => setName(event.target.value)}
             />
             <input
               type="text"
               placeholder="Digite seu e-mail"
               className="bg-gray-900 roundex px-5 h-14"
+              onChange={(event) => setEmail(event.target.value)}
             />
 
             <button
               type="submit"
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+              disabled={loading}
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
               Garantir minha vaga
             </button>
