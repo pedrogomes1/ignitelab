28a9dbc0dce032111c8a75f08164f99f742f09f7
Pedro <pedro-gomes17@hotmail.com>
2022-06-26 22:29:27 -0300

    improvement: added conditional to display video component

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,16 @@
 import { Video } from "../components/Video";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import { useParams } from "react-router-dom";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
         <Sidebar />
       </main>
     </div>
